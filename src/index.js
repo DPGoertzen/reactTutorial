b0efed6523bfd60b43d401c3750b330fb3371986
DPGoertzen <dpgoertzen@gmail.com>
2017-08-21 17:53:22 -0500

    initial buildout of react-redux-es6 tutorial

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDWxQmnqZ-vZ9QuH_M79kOlH3JUEGeVeog';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//create a component that creates/produces html
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// take generated html and apply to DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
